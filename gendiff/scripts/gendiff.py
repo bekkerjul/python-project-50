f867096d04f9d91e7c5b7a699690c5b09d9af071
Julia Bekker <bekker_yulya@mail.ru>
2024-05-22 23:35:43 +0300

    create gendiff.py

---
+++ gendiff/scripts/gendiff.py
@@ -0,0 +1,27 @@
+def make_description():
+    description = """
+    gendiff -h
+    usage: gendiff [-h] first_file second_file
+    
+    Compares two configuration files and shows a difference.
+    
+    positional arguments:
+        first_file
+        second_file
+
+    optional arguments:
+    -h, --help            show this help message and exit
+    """
+    print(description)
+
+
+def main():
+
+
+
+
+
+
+
+if __name__ == '__main__':
+    main()
