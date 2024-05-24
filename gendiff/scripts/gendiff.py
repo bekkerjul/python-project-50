c0b105123cc7697ea9ee297d8083afc66aee5b39
Юлия Беккер <bekker_yulya@mail.ru>
2024-05-24 09:22:26 +0300

    update gendiff.py

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -5,6 +5,7 @@ def main():
     parser = argparse.ArgumentParser(prog='gendiff', description = 'Compares two configuration files and shows a difference')
     parser.add_argument('first_file')
     parser.add_argument('second_file')
+    parser.add_argument('-f', '--format', help='set format of output', default='default')
 
     args = parser.parse_args()
 
