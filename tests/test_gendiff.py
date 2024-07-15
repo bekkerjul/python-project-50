cb9fb8bcc84e6a2740ab69190cf1c37da38628f5
Юлия Беккер <bekker_yulya@mail.ru>
2024-07-15 21:42:31 +0300

    update gendiff.py

--- tests/test_gendiff.py
+++ tests/test_gendiff.py
@@ -1,5 +1,4 @@
-from gendiff.scripts.gendiff import load_format, compare_keys, format_diff, generate_diff, define_format
-import json, yaml
+from gendiff.scripts.gendiff import generate_diff
 
 path1 = 'tests/fixtures/file1.json'
 path2 = 'tests/fixtures/file2.json'
@@ -7,8 +6,14 @@ path3 = 'tests/fixtures/file3.yaml'
 path4 = 'tests/fixtures/file4.yml'
 
 
-def test_generate_diff_json():
-    assert generate_diff(path1, path2) == open('tests/fixtures/result1.txt').read()
+def test_generate_diff_json_stylish():
+    assert generate_diff(path1, path2, stylish) == open('tests/fixtures/result1.txt').read()
 
-def test_generate_diff_yaml():
-    assert generate_diff(path3, path4) == open('tests/fixtures/result2.txt').read()
+
+def test_generate_diff_yaml_stylish():
+    assert generate_diff(path3, path4, stylish) == open('tests/fixtures/result2.txt').read()
+
+
+def test_generate_diff_json_plain():
+    assert generate_diff(path1, path2, plain) == open('tests/fixtures/result3.txt').read()
+
